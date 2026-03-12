62e44e9dd99cc02340d1d0cc03899ff12e5ac7c4
Yi <ge89vod@mytum.de>
2026-03-12 10:53:27 +0100

    modify validation script

---
+++ bash_scripts/valid_dataset_with_diffusion.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+DATASETS=(
+    "real_0_put_bowl_filtered"
+    "real_1_stack_bowls_filtered"
+    "real_0_put_bowl"
+    "real_1_stack_bowls"
+    # "real_2_put_moka_pot"
+    # "real_2_put_moka_pot_filtered"
+    # "real_3_close_drawer"
+    # "real_3_close_drawer_filtered"
+    # "real_4_put_lego_into_drawer"
+    # "real_4_put_lego_into_drawer_filtered"
+    # "real_5_stack_lego"
+    # "real_5_stack_lego_filtered"
+)
+
+for DATASET in "${DATASETS[@]}"; do
+    echo "=========================================="
+    echo "Training diffusion on: ${DATASET}"
+    echo "=========================================="
+    lerobot-train \
+        --job_name="diffusion_fft_${DATASET}" \
+        --output_dir="./outputs/train/diffusion_fft_${DATASET}" \
+        --dataset.repo_id="continuallearning/${DATASET}" \
+        --policy.type=diffusion \
+        --policy.push_to_hub=true \
+        --policy.repo_id="continuallearning/diffusion_fft_${DATASET}" \
+        --batch_size=64 \
+        --num_workers=16 \
+        --steps=40000 \
+        --eval_freq=0 \
+        --save_freq=40000 \
+        --log_freq=100 \
+        --wandb.enable=true \
+        --wandb.disable_artifact=true \
+        --wandb.project=clare_rebuttal \
+        --wandb.entity=470620104-technical-university-of-munich
+done
+
+echo "All training runs completed!"
